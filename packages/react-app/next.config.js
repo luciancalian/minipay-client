c353b2380308824b7441f3c71db23bd102de00f0
Lucian Calian <luciancalian@Lucians-MacBook-Pro.local>
2024-10-17 11:15:10 +0300

    Test minipay

---
+++ packages/react-app/next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      { hostname: "pbs.twimg.com" },
+      { hostname: "cdn-production-opera-website.operacdn.com" },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
